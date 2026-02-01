6a4e083d13e141aecc76b8d6ed8a98b0c47b80f9
Ailu Bosch <ailenbosch@hotmail.com>
2026-02-01 19:20:00 -0300

    Carrito arreglado

--- JavaScript.js
+++ JavaScript.js
@@ -37,7 +37,7 @@ const deportes = [
 ];
 
 let productsContainer = document.getElementById("products-container")
-let cartProducts = []
+let cartProducts = [];
 
 function imprimirProductosEnHTML(deportes) {
     let deport = document.getElementById("Deportes")
@@ -54,78 +54,21 @@ function imprimirProductosEnHTML(deportes) {
 imprimirProductosEnHTML(deportes)
 
         function agregarAlCarrito() {
-            addButton = document.querySelectorAll(".AgregarDeporte")
+            const addButton = document.querySelectorAll(".AgregarDeporte")
             addButton.forEach(button => {
                 button.onclick = (e) => {
-                    const deporteId = e.currentTarget.id
-                    const deporteSeleccionado = deportes.find(deporte => deporte.id == deporteId)
+                    const deporteId = e.currentTarget.id;
+                    const deporteSeleccionado = deportes.find(deporte => deporte.id == deporteId);
 
-                    cartProducts.push(deporteSeleccionado)
-                    console.log(cartProducts)
+                    cartProducts.push(deporteSeleccionado);
+                    console.log(cartProducts);
 
                     localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
                 }
             })
         }
+
 agregarAlCarrito()
-/*function mostrarResultado(elegirDeporte, precioTotal, esSocio) {
-    if (esSocio) {
-        precioTotal = precioTotal - (precioTotal * DescuentoSocios);
-        console.log("El valor final con descuento es de $" + precioTotal);
-        alert(
-            "Deporte seleccionado: " + deportes[elegirDeporte - 1] + "\n" +
-            "Valor final con descuento: $" + precioTotal + "\n\n" +
-            "¡Gracias por ser parte del " + club + "!"
-        );
-    }
-    else {
-        console.log("Valor de la cuota mensual sin descuento: $" + precioTotal)
-        alert(
-            "Deporte seleccionado: " + deportes[elegirDeporte - 1] + "\n" +
-            "Valor de la cuota mensual: $" + precioTotal + "\n\n" +
-            "Hacete socio y no te pierdas un 20% de descuento"
-        )
-    }
-}
 
-while (continuar) {
-    let elegirDeporte = parseInt(prompt("Ingrese:" + "\n" +
-        "1 para inscribirse en Futbol" + "\n" +
-        "2 para inscribirse en Natacion" + "\n" +
-        "3 para inscribirse en Tenis" + "\n" +
-        "4 para inscribirse en Basquet" + "\n" +
-        "5 si desea salir"))
-
-    switch (elegirDeporte) {
-        case 1:
-
-        case 2:
-
-        case 3:
-
-        case 4:
-            precioTotal = precios[elegirDeporte - 1];
-            let esSocio = confirm("¿Ya sos socio del club?");
-            mostrarResultado(elegirDeporte, precioTotal, esSocio);
-            break;
-
-        case 5:
-            continuar = false;
-            console.log("¡Gracias por consultar!");
-            break;
-
-        default:
-            alert("Opción Incorrecta")
-            break;
-    }
-
-    let confirmation = prompt("¿Desea consultar por otro deporte? (si/no)").toLowerCase()
-    if (confirmation == "no") {
-        continuar = false
-        console.log("¡Gracias por consultar!")
-    }
-
-}
-*/
 
 
