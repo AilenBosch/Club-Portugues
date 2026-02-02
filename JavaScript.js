56dbf4e05a2ece5dbc6e0191ea40bf0f6e12c5cc
Ailu Bosch <ailenbosch@hotmail.com>
2026-02-01 21:06:08 -0300

    Agrego imágenes a las cards

--- JavaScript.js
+++ JavaScript.js
@@ -11,24 +11,28 @@ let continuar = true
 // ARRAYS // 
 const deportes = [
     {
+        imagen: "./Assets/Fútbol.png",
         id: 1,
         deporte: "Futbol",
         precio: 30000,
     },
 
     {
+        imagen: "./Assets/Natación.png",
         id: 2,
         deporte: "Natacion",
         precio: 40500,
     },
 
     {
+        imagen: "./Assets/Tenis.png",
         id: 3,
-        deporte: "Tennis",
+        deporte: "Tenis",
         precio: 25500,
     },
 
     {
+        imagen: "./Assets/Basquet.png",
         id: 4,
         deporte: "Basquet",
         precio: 30000,
@@ -44,29 +48,29 @@ function imprimirProductosEnHTML(deportes) {
     deportes.forEach(deporte => {
         const card = document.createElement("div")
         card.className = "Card"
-        card.innerHTML = `<h3> Deporte: ${deporte.deporte}</h3>
+        card.innerHTML = ` <img src="${deporte.imagen}" alt="${deporte.deporte}" class="img-deporte">
+                        <h3> Deporte: ${deporte.deporte}</h3>
                         <h4> Precio: ${deporte.precio}</h4>
                         <button class= "AgregarDeporte" id="${deporte.id}">Agregar</button>`
         deport.appendChild(card)
-            })
+    })
 }
 
 imprimirProductosEnHTML(deportes)
 
-        function agregarAlCarrito() {
-            const addButton = document.querySelectorAll(".AgregarDeporte")
-            addButton.forEach(button => {
-                button.onclick = (e) => {
-                    const deporteId = e.currentTarget.id;
-                    const deporteSeleccionado = deportes.find(deporte => deporte.id == deporteId);
+function agregarAlCarrito() {
+    const addButton = document.querySelectorAll(".AgregarDeporte")
+    addButton.forEach(button => {
+        button.onclick = (e) => {
+            const deporteId = e.currentTarget.id;
+            const deporteSeleccionado = deportes.find(deporte => deporte.id == deporteId);
 
-                    cartProducts.push(deporteSeleccionado);
-                    console.log(cartProducts);
+            cartProducts.push(deporteSeleccionado);
 
-                    localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
-                }
-            })
+            localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
         }
+    })
+}
 
 agregarAlCarrito()
 
